9022e1849c715675770997d63e656071dcb2547d
neilrqm <neilrqm@gmail.com>
2016-03-17 18:23:46 -0700

    Added project

---
+++ src/Minute Manager/MinuteManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MinuteManager
+{
+    public partial class MinuteManager : Form
+    {
+        public MinuteManager()
+        {
+            InitializeComponent();
+        }
+    }
+}
